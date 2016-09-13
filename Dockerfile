40487c04c9e923777877d1346df3b0140d9f439a
Ben Heasly <benjamin.heasly@gmail.com>
2016-09-13 09:42:12 -0400

    end of the line for yas3fs

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,31 @@
 # ninjaben/rtb-support
 # 
-# Create an image with enough dependencies to support a mounted-in Matlab and RenderToolbox3.
+# Create an image with enough dependencies to support a mounted-in Matlab and calling Docker from Matlab.
 #
 # This includes the ability to let Matlab launch Docker containers.  This works by building the Docker
-# executable this image, and then mounting in the port to the Docker daemon, from the Docker host. 
+# executable into this image, and then mounting in the port to the Docker daemon running on the Docker host.
 #
-# In general, the command would look like this:
-# docker run --rm \
-# -v "$MATLAB_ROOT":/usr/local/MATLAB/from-host \
-# -v "$MATLAB_LOGS":/var/log/matlab \
-# -v /var/run/docker.sock:/var/run/docker.sock \
-# --mac-address="$MATLAB_MAC_ADDRESS" \
-# ninjaben/rtb-support -r "system('docker ps');exit;"
-#
-# Here is a specific example:
-# docker run --rm -v "/usr/local/MATLAB/R2016a":/usr/local/MATLAB/from-host -v "/home/ben/Desktop/matlog":/var/log/matlab -v /var/run/docker.sock:/var/run/docker.sock --mac-address="68:f7:28:f6:68:a6" ninjaben/rtb-support -r "system('docker run --rm ninjaben/mitsuba-spectral mitsuba -h');exit;"
-#
-# Thaks to Jérôme Petazzoni for advice on launching "peer" containers in Docker
+# Thaks to Jérôme Petazzoni for advice on this style of launching "peer" containers in Docker
 #   https://jpetazzo.github.io/2015/09/03/do-not-use-docker-in-docker-for-ci/
+#
+# Here's an example that runs Matlab in the container, then launches a Docker container from Matlab:
+#
+# docker run \
+#   --rm \
+#   -v "/usr/local/MATLAB/R2016a":/usr/local/MATLAB/from-host \
+#   -v /var/run/docker.sock:/var/run/docker.sock \
+#   --net="host" \
+#   -e "COMMAND=system('docker run hello-world')" \
+#   ninjaben/rtb-support"
+#
+# The entrypoint is the included script rtb-job.sh.  This script can handle additoinal details that we need for
+# running Matlab jobs on AWS, including:
+#   - toolbox setup with the ToolboxToolbox tbUse
+#   - mounting an S3 bucket for input data
+#   - mounting an s3 bucket for output data
+#
+# To set these things up, the script reads configuration from environment variables.  These can be passed to the
+# Docker container with "-e".
 #
 
 FROM ninjaben/matlab-support
--- rtb-job.sh
+++ rtb-job.sh
@@ -3,29 +3,53 @@
 #
 # Environtment variables for support:
 #
-# INPUT_SCRATCH Input scratch folder to create,
-# INPUT_BUCKET S3 bucket to mout at input scratch folder,
-# INPUT_TOPIC ARN of Amazon SNS Topic to use if S3 buket is to be shared.
-#
-# OUTPUT_SCRATCH, OUTPUT_BUCKET, OUTPUT_TOPIC like INPUT, above.
-#
-# STARTUP initialzation script to pass to run('...')
-# TOOLBOXES comma-separated list of toolboxes to pass to tbUse({'...'}),
+# STARTUP initialzation script invoke as run('$STARTUP') (single quotes added for you)
+# TOOLBOXES arguments to pass to tbUse($TOOLBOXES) (no quotes added)
 # COMMAND command string to pass to matlab -r "..."
+#
+# INPUT_SCRATCH Input scratch folder to create
+# INPUT_BUCKET S3 bucket to mout for input scratch folder
+# INPUT_TOPIC ARN of Amazon SNS Topic to use if input bucket is to be shared
+# INPUT_DIRECT_MOUNT if true (default) directly mounts the INPUT_BUCKET at INPUT_SCRATCH,
+#                    otherwise mounts to /tmp, copies data to INPUT_SCRATCH, then unmounts
+#
+# OUTPUT_SCRATCH Output scratch folder to create
+# OUTPUT_BUCKET S3 bucket to mout for output scratch folder
+# OUTPUT_TOPIC ARN of Amazon SNS Topic to use if output bucket is to be shared
+# OUTPUT_DIRECT_MOUNT if true directly mounts the OUTPUT_BUCKET at OUTPUT_SCRATCH,
+#                     otherwise (default) mounts to /tmp, copies data from OUTPUT_SCRATCH, then unmounts
+#
+# INPUT_DIRECT_MOUNT and OUTPUT_DIRECT_MOUNT decide when to read/write data from/to S3 buckets.  This 
+# matters because S3 access goes through a REST API, and might be slow.
+#
+# For input data, the default behavior is to directly mount INPUT_BUCKET at INPUT_SCRATCH 
+# (ie  INPUT_DIRECT_MOUNT=true).  This lets the job read input data lazily/when needed, which seems sane 
+# especially if the job might not read to read the whole bucket.
+#
+# For output data, the default is to let the job write to OUTPUT_SCRATCH with no bucket mounted, then afterwards 
+# (ie OUTPUT_DIRECT_MOUNT=false), then afterwards mount OUTPUT_BUCKET to /tmp can copy over all the data from
+# OUTPUT_SCRATCH.  This seems sane since it lets the job run without depending on the slow S3 API, but we
+# still want to collect all the output data that was produced.
 #
 # 2016 benjamin.heasly@gmail.com
 
 
 # given or default environment
-: ${INPUT_SCRATCH:="./rtb-input-scratch"}
-: ${INPUT_BUCKET:=""}
-: ${INPUT_TOPIC:=""}
-: ${OUTPUT_SCRATCH:="./rtb-output-scratch"}
-: ${OUTPUT_BUCKET:=""}
-: ${OUTPUT_TOPIC:=""}
 : ${STARTUP:=""}
 : ${TOOLBOXES:=""} 
 : ${COMMAND:="ver"}
+
+: ${INPUT_SCRATCH:="/input-scratch"}
+: ${INPUT_BUCKET:=""}
+: ${INPUT_TOPIC:=""}
+: ${INPUT_DIRECT_MOUNT:="true"}
+: ${INPUT_TEMP:=`mktemp -d /tmp/input-temp-XXXXXXXXXX`}
+
+: ${OUTPUT_SCRATCH:="/output-scratch"}
+: ${OUTPUT_BUCKET:=""}
+: ${OUTPUT_TOPIC:=""}
+: ${OUTPUT_DIRECT_MOUNT:="false"}
+: ${OUTPUT_TEMP:=`mktemp -d /tmp/output-temp-XXXXXXXXXX`}
 
 
 # set up scratch dirs and buckets
@@ -34,35 +58,45 @@ function set_up_scratch ()
   SCRATCH=$1
   BUCKET=$2
   TOPIC=$3
+  TEMP=$4
+  DIRECT_MOUNT=$5
   
   echo "Create <$SCRATCH>"
   mkdir -p "$SCRATCH"
   
   if [ -n "$BUCKET" ];
   then
-    if [ -n "$TOPIC" ];
+    if [ "$DIRECT_MOUNT" == "true" ];
     then
-      echo "Mount Bucket <$BUCKET> at <$SCRATCH> with topic <$TOPIC>"
-      yas3fs "$BUCKET" "$SCRATCH" --topic "$TOPIC" --new-queue
-    else
-      echo "Mount Bucket <$BUCKET> at <$SCRATCH> with no topic"
-      yas3fs "$BUCKET" "$SCRATCH"      
+      if [ -n "$TOPIC" ];
+      then
+        echo "Mount Bucket <$BUCKET> at <$SCRATCH> with topic <$TOPIC>"
+        yas3fs "$BUCKET" "$SCRATCH" --topic "$TOPIC" --new-queue --cache-entries 0 --cache-mem-size 0 --cache-disk-size 0 --s3-num 0
+      else
+        echo "Mount Bucket <$BUCKET> at <$SCRATCH> with no topic"
+        yas3fs "$BUCKET" "$SCRATCH"      
+      fi
+    elif [ -n "$TEMP" ]
+    then
+      echo "Mount Bucket <$BUCKET> at <$TEMP>, copy to <$SCRATCH>"
+      yas3fs "$BUCKET" "$TEMP" --cache-entries 0 --cache-mem-size 0 --cache-disk-size 0 --s3-num 0
+      cp -rf "$TEMP"/* "$SCRATCH/"
+      fusermount -u "$TEMP"
     fi
   fi
 }
 
-set_up_scratch $INPUT_SCRATCH $INPUT_BUCKET INPUT_TOPIC
-set_up_scratch $OUTPUT_SCRATCH OUTPUT_BUCKET OUTPUT_TOPIC
+set_up_scratch "$INPUT_SCRATCH" "$INPUT_BUCKET" "$INPUT_TOPIC" "$INPUT_TEMP" "$INPUT_DIRECT_MOUNT"
+set_up_scratch "$OUTPUT_SCRATCH" "$OUTPUT_BUCKET" "$OUTPUT_TOPIC" "" "$OUTPUT_DIRECT_MOUNT"
 
 
 # build command and call matlab
 MATLAB_LOG="$OUTPUT_SCRATCH"/matlab.log
-MATLAB_COMMAND="try; run('STARTUP'); tbUse($TOOLBOXES); $COMMAND; catch err; disp(err); end; exit();"
+MATLAB_COMMAND="try; run('$STARTUP'); if exist('tbUse', 'file'); tbUse($TOOLBOXES); end; $COMMAND; catch err; disp(err); end; exit();"
 
+
+# invoke Matlab with up-to-date libstdc++, not older version shipped with Matlab
 echo "Call matlab with command <$MATLAB_COMMAND> log to <$MATLAB_LOG>"
-
-
-# load up-to-date libstdc++, not older version shipped with Matlab
 LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6 matlab -nodisplay -logfile "$MATLAB_LOG" -r "$MATLAB_COMMAND"
 
 
@@ -71,14 +105,25 @@ function clean_up_scratch ()
 {
   SCRATCH=$1
   BUCKET=$2
+  TEMP=$3
+  DIRECT_MOUNT=$4
 
   if [ -n "$BUCKET" ];
   then
-    echo "Unmount Bucket <$BUCKET> at <$SCRATCH>"
-    fusermount -u "$SCRATCH"
+    if [ "$DIRECT_MOUNT" == "true" ];
+    then
+      echo "Unmount Bucket <$BUCKET> at <$SCRATCH>"
+      fusermount -u "$SCRATCH"
+    elif [ -n "$TEMP" ]
+    then
+      echo "Mount Bucket <$BUCKET> at <$TEMP>, copy from <$SCRATCH>"
+      yas3fs "$BUCKET" "$TEMP" --cache-entries 0 --cache-mem-size 0 --cache-disk-size 0 --s3-num 0
+      cp -rf "$SCRATCH"/* "$TEMP/"
+      fusermount -u "$TEMP"
+    fi
   fi
 }
 
-clean_up_scratch $INPUT_SCRATCH INPUT_BUCKET
-clean_up_scratch $OUTPUT_SCRATCH OUTPUT_BUCKET
+clean_up_scratch "$INPUT_SCRATCH" "$INPUT_BUCKET" "" "$INPUT_DIRECT_MOUNT"
+clean_up_scratch "$OUTPUT_SCRATCH" "$OUTPUT_BUCKET" "$OUTPUT_TEMP" "$OUTPUT_DIRECT_MOUNT"
 
