5c5d996593c952f84f3ed0cba9162da1864e00d0
Ben Heasly <benjamin.heasly@gmail.com>
2016-10-17 13:34:40 -0400

    Support mounting S3 bucket during jobs.

--- docker/rtb-job.sh
+++ docker/rtb-job.sh
@@ -15,6 +15,15 @@
 # OUTPUT_BUCKET S3 bucket to receive data from OUTPUT_SCRATCH after running COMAND
 # OUTPUT_FLAGS flags to pass to "aws s3 cp ...", default is "--recursive"
 #
+# MOUNT_SCRATCH Scratch folder to create where an S3 bucket can be mounted
+# MOUNT_BUCKET S3 bucket mount at MOUNT_SCRATCH before running COMMAND
+# MOUNT_FLAGS flags to pass to "yas3fs ...", default is "", no special flags
+#
+# Note on mounting buckets: it's good for reading input data from a bucket,
+# especially for lazy reads where we want to avoid copying the whole bucket.
+# It's not so good for writing output data because the job might terminate
+# before the write finishes.  OUTPUT_BUCKET is better for writing.
+#
 # 2016 benjamin.heasly@gmail.com
 
 
@@ -31,6 +40,10 @@
 : ${OUTPUT_BUCKET:=""}
 : ${OUTPUT_FLAGS:="--recursive"}
 
+: ${MOUNT_SCRATCH:="/mount-scratch"}
+: ${MOUNT_BUCKET:=""}
+: ${MOUNT_FLAGS:=""}
+
 
 # util to move data to/from s3
 function copy_data ()
@@ -41,16 +54,50 @@ function copy_data ()
   
   if [ -n "$SOURCE" ] && [ -n "$DESTINATION" ];
   then
-    echo "Copy <$SOURCE> to bucket <$DESTINATION>"
+    echo "Copy <$SOURCE> to <$DESTINATION>"
     aws s3 cp "$SOURCE" "$DESTINATION" "$FLAGS"
   fi
 }
 
 
+# util to mount an S3 bucket
+function mount_bucket ()
+{
+  BUCKET=$1
+  SCRATCH=$2
+  FLAGS=$3
+  
+  if [ -n "$BUCKET" ] && [ -n "$SCRATCH" ];
+  then
+    echo "Mount bucket <$BUCKET> at <$SCRATCH>"
+    yas3fs "$BUCKET" "$SCRATCH" "$FLAGS"
+  fi
+}
+
+
+# util to unmount an S3 bucket
+function unmount_bucket ()
+{
+  BUCKET=$1
+  SCRATCH=$2
+  
+  if [ -n "$BUCKET" ] && [ -n "$SCRATCH" ];
+  then
+    echo "Unount bucket <$BUCKET> at <$SCRATCH>"
+    fusermount -u "$SCRATCH"
+  fi
+}
+
+
 # want scratch dirs to exist
-echo "Create <$INPUT_SCRATCH> and <$OUTPUT_SCRATCH>"
+echo "Create <$INPUT_SCRATCH>, <$OUTPUT_SCRATCH>, and<$MOUNT_SCRATCH>"
 mkdir -p "$INPUT_SCRATCH"
 mkdir -p "$OUTPUT_SCRATCH"
+mkdir -p "$MOUNT_SCRATCH"
+
+
+# mount the bucket
+mount_bucket "$MOUNT_SCRATCH" "$MOUNT_SCRATCH" "$MOUNT_FLAGS"
 
 
 # read data from input bucket
@@ -70,3 +117,6 @@ LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6 matlab -nodisplay -logfile "
 # write data to output bucket
 copy_data "$OUTPUT_SCRATCH" "$OUTPUT_BUCKET" "$OUTPUT_FLAGS"
 
+
+# unmount scratch bucket
+unmount_bucket "$MOUNT_SCRATCH" "$MOUNT_SCRATCH"
